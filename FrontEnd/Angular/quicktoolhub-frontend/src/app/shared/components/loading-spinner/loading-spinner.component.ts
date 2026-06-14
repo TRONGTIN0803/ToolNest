0c3bf42fa7a1a0bcca956a9027b24de98de471cd
Trong Tin <phamtrongtin0803@gmail.com>
2026-06-14 20:23:28 +0700

    Remove legacy prototypes from main

--- FrontEnd/Angular/quicktoolhub-frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loading-spinner',
-  templateUrl: './loading-spinner.component.html',
-})
-export class LoadingSpinnerComponent {}
--- FrontEnd/Angular/quicktoolhub-frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  siteUrl: 'https://quicktoolhub.com',
-  apiUrl: 'http://localhost/utility-tools-hub/wp-json/uth/v1',
-  useMockData: true,
-};
